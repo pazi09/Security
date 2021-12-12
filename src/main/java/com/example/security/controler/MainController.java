644869a48d5ae625c75d1ee78a66b4c0b092a9fe
pazi09 <youngsaintforwait@gmail.com>
2021-12-12 18:14:31 +0400

    Initial commit

---
+++ src/main/java/com/example/security/controler/MainController.java
@@ -0,0 +1,17 @@
+package com.example.security.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        return "hello";
+    }
+
+
+
+}
